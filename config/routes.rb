0182421373d613ff93281b0a421292638fe47ef0
Michael Rickert <mrickert@calipercorp.com>
2013-11-22 12:51:07 -0500

    fix mistype with contact route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Livepost::Application.routes.draw do
   
   match '/help',  to: 'static_pages#help', via: :get
   match '/about', to: 'static_pages#about', via: :get
-  match '/contact', to: 'stati_pages#contact', via: :get
+  match '/contact', to: 'static_pages#contact', via: :get
   root  to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
