8c79ba1f4c77917d9a784e547d0700f967f12b6e
slacker87 <racethesunlive@yahoo.com>
2013-11-28 02:00:50 -0500

    finish faye tutorial from railscasts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
   
   
 
-def broadcast(channel, &block)
-  message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-  uri = URI.parse("http://localhost:9292/faye")
-  Net::HTTP.post_form(uri, :message => message.to_json)
-end
+  def broadcast(channel, &block)
+    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  end
 
 
   
