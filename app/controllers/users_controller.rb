b40182cd4394f62a7f07ffd9c8f28010b65909d1
kcombs <bezzlem@gmail.com>
2013-11-29 16:11:11 -0500

    Create users and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
+	
+	def index
+		@user
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome to LivePost!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User deleted."
+		redirect_to users_url
+	end
+
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	# Before filters
+
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_url, notice: "Please sign in."
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
+	end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Livepost::Application.routes.draw do
-  
-  
-  match '/help',  to: 'static_pages#help', via: :get
-  match '/about', to: 'static_pages#about', via: :get
-  match '/contact', to: 'static_pages#contact', via: :get
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/help',  to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   root  to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
