88bfff9365ddd954d6330cfa8fcf686be71b7f3d
slacker87 <racethesunlive@yahoo.com>
2013-11-30 00:59:45 -0500

    fix mistype

--- app/assets/javascripts/faye.js
+++ app/assets/javascripts/faye.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://livepost-backend/faye');
+  var faye = new Faye.Client('http://livepost-backend.herokuapp.com/faye');
   faye.subscribe('/chatlogs/update', function (data) {
     eval(data);
   });
