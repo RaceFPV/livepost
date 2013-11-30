c3f8a68f0a481bd3bb8c0e1bba5d033dec276d19
slacker87 <racethesunlive@yahoo.com>
2013-11-30 00:56:02 -0500

    get faye to work with heroku

--- app/assets/javascripts/faye.js
+++ app/assets/javascripts/faye.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://livepost.herokuapp.com:9292/faye');
+  var faye = new Faye.Client('http://livepost-backend/faye');
   faye.subscribe('/chatlogs/update', function (data) {
     eval(data);
   });
