28ad6c293a47aaf8b24b238517310f72527c3216
slacker87 <racethesunlive@yahoo.com>
2013-11-30 00:57:39 -0500

    fix mistype

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
 
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://livepost-backend/faye")
+    uri = URI.parse("http://livepost-backend.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
