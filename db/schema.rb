a6dfeeccc807772015811c37ee9112e32666bd9b
slacker87 <racethesunlive@yahoo.com>
2013-12-03 15:54:24 -0500

    add gravatars and place them next to each users post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128185335) do
+ActiveRecord::Schema.define(version: 20131203201842) do
 
   create_table "chatlogs", force: true do |t|
     t.text     "chatname"
