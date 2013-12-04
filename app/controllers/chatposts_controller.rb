789d52de068ddf265a11b45ae892ed939268eeb9
slacker87 <racethesunlive@yahoo.com>
2013-12-04 16:04:21 -0500

    more heroku faye work

--- app/controllers/chatposts_controller.rb
+++ app/controllers/chatposts_controller.rb
@@ -8,16 +8,11 @@ def create
   @chat = @chatlog
   
   @chatposts = @chatlog.chatpost.all
-  
-  #redirect_to @chatlog, :flash => {:success => "Successfully sent message"}
-  # stuff for ajax, not yet working
-    flash[:success] = "Successfully sent message"
   respond_to do |format|
     if @chatpost.save
     flash[:success] = "Successfully sent message"
     format.html { redirect_to @chatlog }
     format.js   {}
-    format.json { render json: @chatpost, status: :created, location: @chatpost}
     end
     end
 end
