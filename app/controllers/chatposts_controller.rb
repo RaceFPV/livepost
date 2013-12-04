ab6c43eb4a0c9ac499e040f82c4dca2648083feb
slacker87 <racethesunlive@yahoo.com>
2013-12-04 02:00:44 -0500

    more work on limiting broadcasts

--- app/controllers/chatposts_controller.rb
+++ app/controllers/chatposts_controller.rb
@@ -1,5 +1,26 @@
 class ChatpostsController < ApplicationController
 
+def update
+  @chatlog = Chatlog.find(params[:id])
+  @chatpost = @chatlog.chatpost.new(params[:chatpost])
+  @chatpost[:username] = current_user.name
+  @chatpost[:post] = params[:chatparams][:post]
+  @chat = @chatlog
+  
+  @chatposts = @chatlog.chatpost.all
+  
+  #redirect_to @chatlog, :flash => {:success => "Successfully sent message"}
+  # stuff for ajax, not yet working
+    flash[:success] = "Successfully sent message"
+  respond_to do |format|
+    if @chatpost.save
+    flash[:success] = "Successfully sent message"
+    format.html { redirect_to @chatlog }
+    format.js   {}
+    format.json { render json: @chatpost, status: :created, location: @chatpost}
+    end
+    end
+end
 
 def show
   @chatlog = Chatlog.find(params[:id])
