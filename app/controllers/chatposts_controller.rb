5a522da8e4aeb45edb2b539603228ecdb92a1266
slacker87 <racethesunlive@yahoo.com>
2013-12-04 00:11:45 -0500

    more work on limiting broadcasts

--- app/controllers/chatposts_controller.rb
+++ app/controllers/chatposts_controller.rb
@@ -7,17 +7,20 @@ class ChatpostsController < ApplicationController
   def create
   @chatlog = Chatlog.find(params[:id])
   @chatpost = @chatlog.chatpost.new(params[:chatpost])
-  @chatpost[:username] = @current_user
+  @chatpost[:username] = current_user.name
   @chatpost[:post] = params[:chatparams][:post]
+  
+  @chatposts = @chatlog.chatpost.all
   
   #redirect_to @chatlog, :flash => {:success => "Successfully sent message"}
   # stuff for ajax, not yet working
+    flash[:success] = "Successfully sent message"
   respond_to do |format|
     if @chatpost.save
     flash[:success] = "Successfully sent message"
     format.html { redirect_to @chatlog }
     format.js   {}
-    format.json { render json: @chatlog, status: :created, location: @chatpost}
+    format.json { render json: @chatpost, status: :created, location: @chatpost}
     end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Livepost::Application.routes.draw do
   match '/signin', to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
   resources :chatlogs, param: :id do
-  match '/chatlogs/:id/update', 'chatlogs#update', via: 'patch'
     member do
       resources :chatposts
     end
