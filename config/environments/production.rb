10eabf4402cbc136814ebfe1548e717c11889288
slacker87 <racethesunlive@yahoo.com>
2013-12-04 23:45:32 -0500

    unfuck heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Livepost::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -12,7 +12,7 @@ Livepost::Application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -33,7 +33,7 @@ Livepost::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.3'
+  config.assets.version = '1.4'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
