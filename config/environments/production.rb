d6713acb78624260a6061bf892b203214d4ceda0
slacker87 <racethesunlive@yahoo.com>
2013-12-05 21:43:52 -0500

    heroku work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Livepost::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.assets.debug = true
+  config.assets.debug = false
 end
