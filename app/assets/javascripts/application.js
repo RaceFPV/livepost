5b3d1c526495356c2bca60cabdfa2d01b1be74e1
slacker87 <racethesunlive@yahoo.com>
2013-12-08 12:18:09 -0500

    fix sidemenu bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require sidebar
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap3
 //= require bootstrap
