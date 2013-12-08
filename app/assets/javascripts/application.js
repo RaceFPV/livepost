25b595430387ff7b0ec319ec94a8a4250c3c6362
slacker87 <racethesunlive@yahoo.com>
2013-12-08 01:52:03 -0500

    precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.bootstrap3
 //= require bootstrap
 //= require turbolinks
 //= require private_pub
