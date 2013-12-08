5ece261a8efbf9d17134cf74b15f6f1dc2bb6bdd
slacker87 <racethesunlive@yahoo.com>
2013-12-08 02:08:39 -0500

    fix some divs

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,7 +1,6 @@
 	
  $(function() {
     $('#datatable').dataTable( {
-    	 "iDisplayLength": 15,
-    	 "aLengthMenu": [[15, 25, 50, -1], [15, 25, 50, "All"]]
+    	 "iDisplayLength": 99999
     });
   } );
