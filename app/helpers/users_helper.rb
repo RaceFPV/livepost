414536a61ea815d7385a8f40f0feb19db558e687
Harrison Gill <harrisongill@mac.com>
2013-12-09 17:10:52 +0100

    Add social media profile links to user profile (untested)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,5 +6,17 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def twitter_for(user)
+  	twitter_url = "https://www.twitter.com/#{twitter_id}"
+  end
+
+  def facebook_for(user)
+  	facebook_url = "#{facebook}"
+  end
+
+  def linkedin_for(user)
+	linkedin_url = "#{linkedin}"
+  end
   
 end
