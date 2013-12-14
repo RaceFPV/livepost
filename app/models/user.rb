37f9afa89579b4fec93f1ce3a4ca7d3141ed8ffa
Stuart Hannig <stuarthannig@gmail.com>
2013-12-14 04:17:38 +0000

    Add user id to Chatlog schema & switch code over to use as foreign key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	has_many :chatpost, dependent: :destroy
+  
+  before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	attr_accessor :twitter_id, :facebook, :linkedin, :location
 
