99d3d32435a0f94a8bc2e6208b2721042e03bc5a
slacker87 <racethesunlive@yahoo.com>
2013-12-22 18:16:18 +0000

    add basic structure for previewing latest post while in browse chat page

--- app/assets/javascripts/popovers.js
+++ app/assets/javascripts/popovers.js
@@ -2,5 +2,5 @@
 $(function() {
 	$('[data-toggle="popover-right"]').popover({'placement': 'right', html: 'true'});
 	$('[data-toggle="popover-bottom"]').popover({'placement': 'bottom', html: 'true'});
-	$('[data-toggle="popover-top"]').popover({trigger: 'hover', 'placement': 'top'});
+  $('[data-toggle="popover-top"]').popover({trigger: 'hover', 'placement': 'top', html: 'true'});
 });
