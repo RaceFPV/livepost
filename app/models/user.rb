c93591c62822313412985f65d27248310298982e
harrisongill <harrisongill@mac.com>
2013-12-28 20:38:26 +0000

    Add views from social_stream

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :chatpost, dependent: :destroy
+    acts_as_messageable
+
   
   before_save { self.email = email.downcase }
 	before_create :create_remember_token
